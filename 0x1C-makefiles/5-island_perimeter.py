6a541fc937daa03aa556a7d56bf938dbd06ff9d0
bullbearan <bullbearan@gmail.com>
2022-07-14 13:38:33 +0100

    test

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,14 +5,22 @@
 def island_perimeter(grid):
     '''This is a function'''
     p = 0
-    for i in range(1, len(grid) - 1):
-        for j in range(1, len(grid[i]) - 1):
-            if grid[i][j] == 1 and grid[i][j - 1] == 0:
-                p += 1
-            if grid[i][j] == 1 and grid[i - 1][j] == 0:
-                p += 1
-            if grid[i][j] == 1 and grid[i][j + 1] == 0:
-                p += 1
-            if grid[i][j] == 1 and grid[i + 1][j] == 0:
-                p += 1
+    if len(grid) > 1:
+        for i in range(1, len(grid) - 1):
+            for j in range(1, len(grid[i]) - 1):
+                if grid[i][j] == 1 and grid[i][j - 1] == 0:
+                    p += 1
+                if grid[i][j] == 1 and grid[i - 1][j] == 0:
+                    p += 1
+                if grid[i][j] == 1 and grid[i][j + 1] == 0:
+                    p += 1
+                if grid[i][j] == 1 and grid[i + 1][j] == 0:
+                    p += 1
+    else:
+        for i in grid:
+            for j in range(1, len(grid[0] - 1)):
+                    if grid[0][j] == 1 and grid[0][j - 1] == 0:
+                        p += 1
+                    if grid[0][j] == 1 and grid[0][j + 1] == 0:
+                        p += 1
     return p
