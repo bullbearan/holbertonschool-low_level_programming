ad08c38ee4223df2482743e283e5cdec749b6a5d
bullbearan <bullbearan@gmail.com>
2022-07-14 13:44:30 +0100

    fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,22 +5,12 @@
 def island_perimeter(grid):
     '''This is a function'''
     p = 0
-    if len(grid) > 1:
-        for i in range(1, len(grid) - 1):
-            for j in range(1, len(grid[i]) - 1):
-                if grid[i][j] == 1 and grid[i][j - 1] == 0:
-                    p += 1
-                if grid[i][j] == 1 and grid[i - 1][j] == 0:
-                    p += 1
-                if grid[i][j] == 1 and grid[i][j + 1] == 0:
-                    p += 1
-                if grid[i][j] == 1 and grid[i + 1][j] == 0:
-                    p += 1
-    else:
-        for i in grid:
-            for j in range(1, len(grid[0] - 1)):
-                    if grid[0][j] == 1 and grid[0][j - 1] == 0:
-                        p += 1
-                    if grid[0][j] == 1 and grid[0][j + 1] == 0:
-                        p += 1
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1: 
+                p += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    p -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    p -= 2
     return p
